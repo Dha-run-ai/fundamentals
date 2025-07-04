b3e8b3f7dbdc8abf631badc7ebf48fdcf0d5586c
Dharun M <dharunm2004@gmail.com>
2025-07-04 20:59:20 +0530

    Add files via upload

---
+++ Checkposorneg.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Checkposorneg
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	
+	if(n>0){
+	    System.out.print("Positive Number");
+	    
+	}
+	else if (n<0){
+	    System.out.print("Negative Number");
+	    
+	} 
+	else if (n==0){
+	    System.out.print("Zero");
+	}
+		
+	}
+}
